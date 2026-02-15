4c6410417d43f1f198f86802a847760c8fcc0b9b
EgorPerf <perfilew.egor@yandex.ru>
2026-02-16 04:47:59 +0700

    Изменение в User

--- src/main/java/org/example/rbac/model/User.java
+++ src/main/java/org/example/rbac/model/User.java
@@ -1,20 +1,22 @@
 package org.example.rbac.model;
 
+import java.util.regex.Pattern;
+
 public record User(String username, String fullName, String email) {
 
-    private static final String USERNAME_REGEX = "^[a-zA-Z0-9_]{3,20}$";
-    private static final String EMAIL_REGEX = "^[^@\\s]+@[^@\\s]+\\.[^@\\s]+$";
+    private static final Pattern USERNAME_PATTERN = Pattern.compile("^[a-zA-Z0-9_]{3,20}$");
+    private static final Pattern EMAIL_PATTERN = Pattern.compile("^[^@\\s]+@[^@\\s]+\\.[^@\\s]+$");
 
     public static User validate(String username, String fullName, String email) {
         if (fullName == null || fullName.isBlank()) {
             throw new IllegalArgumentException("FullName не может быть пустым.");
         }
 
-        if (username == null || !username.matches(USERNAME_REGEX)) {
+        if (username == null || !USERNAME_PATTERN.matcher(username).matches()) {
             throw new IllegalArgumentException("Username должен быть от 3 до 20 символов и содержать только латиницу, цифры или подчёркивание.");
         }
 
-        if (email == null || !email.matches(EMAIL_REGEX)) {
+        if (email == null || !EMAIL_PATTERN.matcher(email).matches()) {
             throw new IllegalArgumentException("Email имеет неверный формат.");
         }
 
